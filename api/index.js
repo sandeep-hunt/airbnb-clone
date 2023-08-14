0a559fc1b05553758aa63a5792701e38cdece545
sanddep-dev <sandeep.n062000@gmail.com>
2023-08-14 15:27:00 +0530

    added login api functionality & registration alert issues

--- api/index.js
+++ api/index.js
@@ -22,11 +22,33 @@ app.get('/test', (req,res) => {
 
 app.post('/register', async(req,res) => {
     const {name,email,password} = req.body;
-    const userDoc = await UserModel.create({
-        name,
-        email,
-        password:bcrypt.hashSync(password, bcryptSalt),
-    });
+    try {
+        const userDoc = await UserModel.create({
+            name,
+            email,
+            password:bcrypt.hashSync(password, bcryptSalt),
+        });
+    }
+    catch(e) {
+        res.status(422).json(e);
+    }
 });
+
+app.post('/login', async (req,res) => {
+    const {email, password} = req.body;
+    const userDoc = await UserModel.findOne({email});
+    if(userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if(passOk) {
+            res.json('ok');
+        }
+        else {
+            res.status(422).json('pass not ok');
+        }
+    }
+    else {
+        res.json('not found');
+    }
+})
 
 app.listen(4000);
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import {Link} from "react-router-dom";
 
 function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault();
+    try {
+      await axios.post('/login', {email, password});
+    }
+    catch (e) {
+      alert('Login failed');
+    }
+  }
   return (
     <React.Fragment>
       <div className='mt-4 grow flex items-center justify-around'>
         <div className="mb-32">
           <h1 className="text-4xl text-center mb-4">Login</h1>
-          <form action="#" className='max-w-md mx-auto'>
-            <input type="email" name="" id="" placeholder='your@email.com' />
-            <input type="password" name="" id="" placeholder='password' />
+          <form action="#" className='max-w-md mx-auto' onSubmit={handleLoginSubmit}>
+            <input type="email" name="" id="" placeholder='your@email.com' value={email} onChange={ev => setEmail(ev.target.value)} />
+            <input type="password" name="" id="" placeholder='password' value={password} onChange={ev => setPassword(ev.target.value)} />
             <button className='primary'>Login</button>
             <div className="text-center py-2 text-gray-500">Dont have an account yet? <Link to={'/register'} className="underline text-black">Register Now</Link></div>
           </form>
